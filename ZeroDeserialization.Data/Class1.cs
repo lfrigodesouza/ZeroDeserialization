f48ccfa8bb2744c48174ee2e9ae9016db3a2f26b
Lucas Frigo de Souza <lfrigodesouza@outlook.com>
2020-11-19 14:07:39 -0300

    Commit inicial

---
+++ ZeroDeserialization.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ZeroDeserialization.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ ZeroDeserialization.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ZeroDeserialization.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
